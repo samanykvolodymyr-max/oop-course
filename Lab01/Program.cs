bf2b0387d10b16a61de9a8b1ecae7bea08bafce8
Саманик Bолодимир <samanyk.volodymyr@chnu.edu.ua>
2026-09-14 22:23:36 +0300

    Lab01 Task05

--- Lab01/Program.cs
+++ Lab01/Program.cs
@@ -19,3 +19,7 @@ else if (choice == "4")
 {
     Task4.Run();
 }
+else if (choice == "5")
+{
+    Task5.Run();
+}
---
+++ Lab01/Task5.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Task5
+{
+    public static void Run()
+    {
+        int dayNumber = int.Parse(Console.ReadLine()!);
+
+        string result = dayNumber switch
+        {
+            1 => "День: Понеділок, 08:00–18:00",
+            2 => "День: Вівторок, 08:00–18:00",
+            3 => "День: Середа, 09:00–17:00",
+            4 => "День: Четвер, 08:00–18:00",
+            5 => "День: П'ятниця, 08:00–16:00",
+            6 => "День: Субота, 09:00–14:00",
+            7 => "День: Неділя — вихідний",
+            _ => "День: невідомий день"
+        };
+
+        Console.WriteLine(result);
+    }
+}
