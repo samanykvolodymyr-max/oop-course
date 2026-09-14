00737e185a9ba5a3b84ec89592e67392d143c352
Саманик Bолодимир <samanyk.volodymyr@chnu.edu.ua>
2026-09-14 23:03:39 +0300

    Lab01 Task06

---
+++ Lab01/Task6.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class Task6
+{
+    public static void Run()
+    {
+        int cardNumber = int.Parse(Console.ReadLine()!);
+
+        int lastDigit = cardNumber % 10;
+
+        string department = lastDigit switch
+        {
+            0 or 1 => "загальна терапія",
+            2 or 3 => "хірургія",
+            4 or 5 => "кардіологія",
+            6 or 7 => "неврологія",
+            8 or 9 => "офтальмологія",
+            _ => "невідомо"
+        };
+
+        string isDiscount = (cardNumber % 2 == 0) ? "так" : "ні";
+        string isCheckup = (cardNumber % 3 == 0) ? "так" : "ні";
+
+        Console.WriteLine($"Відділення: {department}");
+        Console.WriteLine($"Пільгова:   {isDiscount}");
+        Console.WriteLine($"Огляд:      {isCheckup}");
+    }
+}
