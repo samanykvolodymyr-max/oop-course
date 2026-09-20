6ac612899994568f0a232469ad32cf6f6e779d2f
Саманик Bолодимир <samanyk.volodymyr@chnu.edu.ua>
2026-09-20 15:57:31 +0300

    Lab02 Task01

--- Lab02/Program.cs
+++ Lab02/Program.cs
@@ -1,1 +1,6 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Globalization;
+
+System.Threading.Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+
+Task1.Run();
---
+++ Lab02/Task1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+public class Task1
+{
+    public static void Run()
+    {
+        int n = int.Parse(Console.ReadLine()!);
+        double[] arr = new double[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            arr[i] = double.Parse(Console.ReadLine()!);
+        }
+
+        double sum = 0;
+        double min = arr[0];
+        double max = arr[0];
+
+        for (int i = 0; i < n; i++)
+        {
+            sum += arr[i];
+
+            if (arr[i] < min)
+            {
+                min = arr[i];
+            }
+            if (arr[i] > max)
+            {
+                max = arr[i];
+            }
+        }
+
+        double avg = sum / n;
+        int count = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (arr[i] > avg)
+            {
+                count++;
+            }
+        }
+
+        Console.WriteLine($"Кількість: {n} / Середня вага: {avg:F1} кг / Мін / Макс: {min:F1} / {max:F1} кг / Вище середнього: {count} з {n}");
+    }
+}
