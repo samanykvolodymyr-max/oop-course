078450d6a535d22223ed7ce28a5e2669e3038058
Саманик Bолодимир <samanyk.volodymyr@chnu.edu.ua>
2026-09-20 17:13:05 +0300

    Lab02 Task07

--- Lab02/Program.cs
+++ Lab02/Program.cs
@@ -12,3 +12,4 @@ else if (choice == "3") Task3.Run();
 else if (choice == "4") Task4.Run();
 else if (choice == "5") Task5.Run();
 else if (choice == "6") Task6.Run();
+else if (choice == "7") Task7.Run();
---
+++ Lab02/Task7.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+public class Task7
+{
+    public static void Run()
+    {
+        int n = int.Parse(Console.ReadLine()!);
+
+        string[] names = new string[n];
+        double[] bmis = new double[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            names[i] = Console.ReadLine()!;
+            bmis[i] = double.Parse(Console.ReadLine()!);
+        }
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = 0; j < n - 1 - i; j++)
+            {
+                if (bmis[j] < bmis[j + 1])
+                {
+                    double tempBmi = bmis[j];
+                    bmis[j] = bmis[j + 1];
+                    bmis[j + 1] = tempBmi;
+
+                    string tempName = names[j];
+                    names[j] = names[j + 1];
+                    names[j + 1] = tempName;
+                }
+            }
+        }
+
+        Console.WriteLine("=== Рейтинг ІМТ ===");
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine($"#{i + 1} {names[i]}: {bmis[i]:F2}");
+        }
+    }
+}
