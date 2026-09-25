f3f24124165ee10c0ea8339139a8c22095474e20
Саманик Bолодимир <samanyk.volodymyr@chnu.edu.ua>
2026-09-25 22:32:27 +0300

    Lab03 Task01

---
+++ ClinicApp/Patient.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace ClinicApp;
+
+public class Patient
+{
+    private static int _nextId = 1;
+
+    public int Id { get; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public string BloodType { get; set; }
+    public string Phone { get; set; }
+    public string Email { get; set; }
+
+    public string FullName
+    {
+        get { return FirstName + " " + LastName; }
+    }
+
+    public int Age
+    {
+        get
+        {
+            int age = DateTime.Today.Year - DateOfBirth.Year;
+            if (DateOfBirth.Date > DateTime.Today.AddYears(-age))
+            {
+                age--;
+            }
+            return age;
+        }
+    }
+
+    public bool IsAdult
+    {
+        get { return Age >= 18; }
+    }
+
+    public Patient()
+        : this("Невідомий", "Пацієнт", new DateTime(1998, 1, 1), "Невідомо", "0000000000")
+    {
+    }
+
+    public Patient(string firstName, string lastName)
+        : this(firstName, lastName, new DateTime(1998, 1, 1), "Невідомо", "0000000000")
+    {
+    }
+
+    public Patient(string firstName, string lastName, DateTime dob, string bloodType, string phone)
+    {
+        Id = _nextId;
+        _nextId++;
+        FirstName = firstName;
+        LastName = lastName;
+        DateOfBirth = dob;
+        BloodType = bloodType;
+        Phone = phone;
+        Email = "";
+    }
+
+    public string GetAgeCategory()
+    {
+        if (Age < 18)
+        {
+            return "дитина";
+        }
+        if (Age < 60)
+        {
+            return "дорослий";
+        }
+        return "літній";
+    }
+
+    public override string ToString()
+    {
+        return $"[{Id}] {FullName} | Вік: {Age} ({GetAgeCategory()}) | Кров: {BloodType} | Тел: {Phone}";
+    }
+}
--- ClinicApp/Program.cs
+++ ClinicApp/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using ClinicApp;
+
+Patient p1 = new Patient("Іван", "Петренко", new DateTime(1983, 5, 14), "A+", "0501234567");
+Patient p2 = new Patient("Олена", "Коваль", new DateTime(1991, 11, 20), "B-", "0672345678");
+Patient p3 = new Patient("Максим", "Бойко", new DateTime(2010, 3, 10), "O+", "0933456789");
+Patient p4 = new Patient();
+Patient p5 = new Patient("Марія", "Ткач");
+
+Console.WriteLine(p1);
+Console.WriteLine(p2);
+Console.WriteLine(p3);
+Console.WriteLine(p4);
+Console.WriteLine(p5);
